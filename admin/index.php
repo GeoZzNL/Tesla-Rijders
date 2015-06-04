33f0382e34f4f1731be7b32caaf6fe4c8e2d66d4
Tom Heek <tdheek@gmail.com>
2015-06-04 15:26:37 +0200

    new

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,9 @@
     $fetcht         = $gett->fetch(PDO::FETCH_ASSOC);
     $imagequery     = $handler->query("SELECT * FROM images");
     
-    $version = 'CMS Version 1.2';
+    $users = $handler->query('SELECT * FROM users');
+    
+    $fetch = $users->fetchall(PDO::FETCH_COLUMN, 1);
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -16,7 +18,7 @@
 	<meta name="author" content="Tom Heek" />
     <link href="main.css" rel="stylesheet" type="text/css" />
     
-	<title><?php echo $version; ?></title>
+	<title>CMS</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src="ckeditor/ckeditor.js"></script>
@@ -26,6 +28,87 @@
     <header>
 	   <div class="font3"><?php echo sname($fetcht); ?> - CMS</div>
     </header>
+    <?php
+        if(isset($_SESSION['admin']) && in_array($_SESSION['admin'], $fetch)){
+    ?>
+        <nav class="main-menu">
+            <ul>
+                <li>
+                    <a href="index.php?p=cms">
+                        <i class="fa fa-home fa-2x"></i>
+                        <span class="nav-text">
+                            Dashboard
+                        </span>
+                    </a>
+                  
+                </li>
+                <li class="has-subnav">
+                    <a href="index.php?p=cms&do=add">
+                        <i class="fa fa-plus fa-2x"></i>
+                        <span class="nav-text">
+                            Pagina toevoegen
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="index.php?p=cms&do=edit">
+                       <i class="fa fa-edit fa-2x"></i>
+                        <span class="nav-text">
+                            Pagina bewerken
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="index.php?p=cms&do=edit&edit=uploadhidden">
+                       <i class="fa fa-upload fa-2x"></i>
+                        <span class="nav-text">
+                            Upload plaatje
+                        </span>
+                    </a>
+                   
+                </li>
+                <li>
+                    <a href="index.php?p=cms&do=gallery">
+                        <i class="fa fa-picture-o fa-2x"></i>
+                        <span class="nav-text">
+                            Foto's beheren
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?p=usermanagement">
+                        <i class="fa fa-users fa-2x"></i>
+                        <span class="nav-text">
+                            Gebruikersbeheer
+                        </span>
+                    </a>
+                </li>
+                <li>
+                   <a href="index.php?p=cms&do=settings">
+                       <i class="fa fa-cog fa-2x"></i>
+                        <span class="nav-text">
+                            Instellingen
+                        </span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="logout">
+                <li>
+                   <a href="index.php?p=cms&do=logout">
+                         <i class="fa fa-power-off fa-2x"></i>
+                        <span class="nav-text">
+                            Logout
+                        </span>
+                    </a>
+                </li>  
+            </ul>
+        </nav>
+        <?php
+            }
+        ?>    
     <section>
     <?php 
         if(isset($_GET['p'])){ 
--- admin/pages/cms.php
+++ admin/pages/cms.php
@@ -1,8 +1,4 @@
 <?php
-$users = $handler->query('SELECT * FROM users');
-
-$fetch = $users->fetchall(PDO::FETCH_COLUMN, 1);
-
 if(isset($_SESSION['admin']) && in_array($_SESSION['admin'], $fetch)){
     if(isset($_GET['do'])){
     if($_GET['p'] == 'cms' && $_GET['do'] == 'logout'){
@@ -10,69 +6,6 @@ if(isset($_SESSION['admin']) && in_array($_SESSION['admin'], $fetch)){
         header('Location: ' . $website_url . 'admin/index.php');
     }
     }
-?>
-<div class="font3"><?php echo $version; ?></div>
-<div class="boxer">
-	<div class="box-row">
-		<div class="box_cms">
-        <a href="index.php?p=cms">
-        <div class="marginpx">
-            <img src="img/home.png" width="64" height="64" alt="Home" /><br />
-            Home
-        </div>
-        </a>
-        </div>
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=add">
-        <div class="marginpx">
-            <img src="img/add.png" width="64" height="64" alt="Add" /><br />
-            Pagina toevoegen
-        </div>
-        </a>
-        </div>
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=edit">
-        <div class="marginpx">
-            <img src="img/edit.png" width="64" height="64" alt="Edit" /><br />
-            Pagina aanpassen
-        </div>
-        </a>
-        </div>
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=edit&edit=uploadhidden">
-        <div class="marginpx">
-            <img src="img/upload.png" width="64" height="64" alt="Upload" /><br />
-            Foto uploaden
-        </div>
-        </a>
-        </div>
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=gallery">
-        <div class="marginpx">
-            <img src="img/gallery.png" width="64" height="64" alt="Gallery" /><br />
-            Gallerij
-        </div>
-        </a>
-        </div>        
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=settings">
-        <div class="marginpx">
-            <img src="img/settings.png" width="64" height="64" alt="Settings" /><br />
-            Instellingen
-        </div>
-        </a>
-        </div>
-		<div class="box_cms">
-        <a href="index.php?p=cms&do=logout">
-        <div class="marginpx">
-            <img src="img/logout.png" width="64" height="64" alt="Logout" /><br />
-            Uitloggen
-        </div>
-        </a>
-        </div>
-	</div>
-</div>
-<?php
     if(empty($_GET['do'])){
 ?>
 <p>U bent ingelogd als Tesla Rijders Admin, u kunt de website beheren met de bovenstaande knoppen.</p>
